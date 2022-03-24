e0ffc7d9b1d255d9fe40c7cbd12a7b51c95d27fc
Julien_P <jparis.py@gmail.com>
2022-03-24 17:22:06 +0100

    starting project

---
+++ .eslintrc.js
@@ -0,0 +1,133 @@
+const INLINE_ELEMENTS = [
+  'a',
+  'abbr',
+  'audio',
+  'b',
+  'bdi',
+  'bdo',
+  'canvas',
+  'cite',
+  'code',
+  'data',
+  'del',
+  'dfn',
+  'em',
+  'i',
+  'iframe',
+  'ins',
+  'kbd',
+  'label',
+  'map',
+  'mark',
+  'noscript',
+  'object',
+  'output',
+  'picture',
+  'q',
+  'ruby',
+  's',
+  'samp',
+  'small',
+  'span',
+  'strong',
+  'sub',
+  'sup',
+  'svg',
+  'time',
+  'u',
+  'var',
+  'video'
+]
+
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    'plugin:vue/essential',
+    'plugin:vue/recommended',
+    // 'plugin:vue/airbnb',
+    '@vue/standard'
+  ],
+  parserOptions: {
+    parser: '@babel/eslint-parser'
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    // 'no-undef': 'off',
+
+    // rules from https://medium.com/@LegendofColt/eslint-for-vue-js-f39194f15beb
+    // 'max-len': [0],
+    // 'import/no-extraneous-dependencies': ['error', {
+    //   'optionalDependencies': ['test/unit/index.js']
+    // }],
+    // 'import/no-unresolved': 'off',
+    // 'no-unused-expressions': ['error', { 'allowTernary': true }],
+    // 'no-param-reassign': 0,
+    // 'quote-props': ['error', 'consistent'],
+
+    // vue rules
+    'vue/order-in-components': ['error', {
+      order: [
+        'el',
+        'name',
+        'parent',
+        'functional',
+        ['delimiters', 'comments'],
+        ['components', 'directives', 'filters'],
+        'extends',
+        'mixins',
+        'inheritAttrs',
+        'model',
+        ['props', 'propsData'],
+        'data',
+        'computed',
+        'watch',
+        'LIFECYCLE_HOOKS',
+        'methods',
+        ['template', 'render'],
+        'renderError'
+      ]
+    }],
+    'vue/html-closing-bracket-newline': ['error', {
+      singleline: 'never',
+      multiline: 'never'
+    }],
+    'vue/html-closing-bracket-spacing': ['error', {
+      startTag: 'never',
+      endTag: 'never',
+      selfClosingTag: 'never'
+    }],
+    'vue/script-indent': ['error', 2, {
+      baseIndent: 0,
+      switchCase: 1,
+      ignores: []
+    }],
+    'vue/max-attributes-per-line': ['error', {
+      singleline: {
+        max: 1
+      },
+      multiline: {
+        max: 1
+      }
+    }],
+    'vue/multiline-html-element-content-newline': ['error', {
+      ignoreWhenEmpty: true,
+      ignores: ['pre', 'textarea', ...INLINE_ELEMENTS],
+      allowEmptyLines: false
+    }]
+  },
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)'
+      ],
+      env: {
+        jest: true
+      }
+    }
+  ]
+}
---
+++ vue.config.js
@@ -0,0 +1,21 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true
+})
+
+// const webpack = require('webpack')
+// module.exports = {
+//   configureWebpack: {
+//     plugins: [
+//       new webpack.optimize.LimitChunkCountPlugin({
+//         maxChunks: 1
+//       })
+//     ]
+//   },
+//   filenameHashing: false,
+//   productionSourceMap: false,
+//   chainWebpack:
+//     config => {
+//       config.optimization.delete('splitChunks')
+//     }
+// }
