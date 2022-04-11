da998c0d3aa06cad4902ea96bf8183de0972c917
Julien_P <jparis.py@gmail.com>
2022-04-11 13:10:17 +0200

    minor css fix to file title

--- src/components/navbar/FileTitle.vue
+++ src/components/navbar/FileTitle.vue
@@ -1,44 +1,46 @@
 <template>
   <div class="FileTitle">
-    <div
-      :class="`dropdown ${hover ? 'is-active' : ''}`"
-      @mouseover="hover = true"
-      @mouseleave="hover = false">
-      <div class="dropdown-trigger">
-        <b-tooltip
-          :label="t('file.fileInfos', locale)"
-          type="is-dark"
-          position="is-right">
-          <div class="is-flex is-align-items-center">
-            <span class="mx-2 is-size-4">
-              {{ title }}
-            </span>
+    <b-tooltip
+      :label="t('file.fileInfos', locale)"
+      type="is-dark"
+      position="is-right">
+      <div class="is-flex is-align-items-center">
+        <span class="mx-2 is-size-4">
+          {{ title }}
+        </span>
+        <div
+          :class="`dropdown ${hover ? 'is-active' : ''}`"
+          @mouseover="hover = true"
+          @mouseleave="hover = false">
+          <div class="dropdown-trigger">
+            <!-- <div class=""> -->
             <b-icon
               size="is-small"
-              class="ml-1 has-text-grey-light"
+              class="ml-1 mr-2 has-text-grey-light"
               icon="information-outline"/>
+            <!-- </div> -->
           </div>
-        </b-tooltip>
-      </div>
-      <div
-        id="dropdown-file-infos"
-        class="dropdown-menu file-infos"
-        role="menu">
-        <div class="dropdown-content px-3 py-3 is-size-6">
-          <p class="has-text-centered has-text-weight-bold">
-            <b-icon
-              size="is-small"
-              class="mr-3 has-text-grey-light"
-              icon="information-outline"/>
-            {{ t('file.fileInfos', locale) }}
-          </p>
-          <!-- TILES -->
-          <GitObjInfos
-            :file-id="fileId"
-            :locale="locale"/>
+          <div
+            id="dropdown-file-infos"
+            class="dropdown-menu file-infos"
+            role="menu">
+            <div class="dropdown-content px-3 py-3 is-size-6">
+              <p class="has-text-centered has-text-weight-bold">
+                <b-icon
+                  size="is-small"
+                  class="mr-3 has-text-grey-light"
+                  icon="information-outline"/>
+                {{ t('file.fileInfos', locale) }}
+              </p>
+              <!-- TILES -->
+              <GitObjInfos
+                :file-id="fileId"
+                :locale="locale"/>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+    </b-tooltip>
   </div>
 </template>
 
@@ -81,6 +83,6 @@ export default {
 
 <style scoped>
   .file-infos {
-    min-width: 45em;
+    min-width: 37em;
   }
 </style>
