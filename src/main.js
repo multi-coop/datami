9fbc34a28b7b0cc05b70d21d6b02002dd54cb0ba
Julien_P <jparis.py@gmail.com>
2022-05-27 15:20:00 +0200

    delete useless comment in main.js

--- src/main.js
+++ src/main.js
@@ -24,8 +24,6 @@ Vue.use(Buefy, {
 GitributeFile.store = store
 GitributeExploWiki.store = store
 
-console.log('>>> Gitribute > main.js > process.env : ', process.env)
-
 Vue.use(vueCustomElement, {
   shadow: true
 })
