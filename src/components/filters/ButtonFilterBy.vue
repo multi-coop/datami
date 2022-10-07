25d540924d6e97ea59b82f60937fd7de5dfca027
Julien_P <jparis.py@gmail.com>
2022-10-07 16:15:23 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- src/components/filters/ButtonFilterBy.vue
+++ src/components/filters/ButtonFilterBy.vue
@@ -8,7 +8,7 @@
         <div class="is-flex">
           <b-tooltip
             :label="t(`filters.filterBy`, locale)"
-            :type=" `${isDarkMode ? 'is-white' : 'is-dark'}`"
+            :type="`${isDarkMode ? 'is-white' : 'is-dark'}`"
             position="is-top">
             <b-icon
               icon="filter"
