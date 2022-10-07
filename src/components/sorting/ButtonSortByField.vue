89410708fb598b31ff4901a9100da3c7f9c50ba6
Lysiane Don <lysiane.don@multi.coop>
2022-10-07 16:08:11 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- src/components/sorting/ButtonSortByField.vue
+++ src/components/sorting/ButtonSortByField.vue
@@ -10,7 +10,7 @@
     <!-- FIELD'S SORT ICON -->
     <b-tooltip
       :label="t(getSortTooltip, locale)"
-      :type=" `${isDarkMode ? 'is-white' : 'is-dark'}`"
+      :type="`${isDarkMode ? 'is-white' : 'is-dark'}`"
       position="is-top"
       append-to-body>
       <b-icon
