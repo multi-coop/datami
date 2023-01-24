011039bf4dc334b1e71b55d957ea0dd76dcf4412
Julien_P <jparis.py@gmail.com>
2023-01-24 21:33:36 +0100

    more on apex charts and big values for cards miniviz

--- src/components/previews/dataviz/DatamiMiniviz.vue
+++ src/components/previews/dataviz/DatamiMiniviz.vue
@@ -22,13 +22,41 @@
         :options="options"
         :series="series"/>
     </div>
+    <div v-if="minivizSettings.viztype === 'barchart-vertical'">
+      <ApexChart
+        ref="barchart-vertical"
+        type="bar"
+        :options="options"
+        :series="series"/>
+    </div>
+    <div v-if="minivizSettings.viztype === 'barchart-horizontal'">
+      <ApexChart
+        ref="barchart-horizontal"
+        type="bar"
+        :options="options"
+        :series="series"/>
+    </div>
+    <div v-if="minivizSettings.viztype === 'big-values'">
+      <span
+        v-for="(val, i) in series"
+        :key="`${val.field.field}-${i}`"
+        class="mx-3">
+        {{ val.field.name }}
+        <span class="tag is-info is-large">
+          {{ val.value }}
+        </span>
+      </span>
+    </div>
 
     <!-- DEBUGGING -->
     <div v-if="true">
+      <hr>
       vizType: <code>{{ vizType }}</code><br>
-      fieldsForSerie: <br><pre><code>{{ fieldsForSerie }}</code></pre><br>
-      series: <br><code>{{ series }}</code><br>
-      options: <br><pre><code>{{ options }}</code></pre><br>
+      <span v-if="vizSpecs.needSerie">series: <br><code>{{ series }}</code></span><br>
+    </div>
+    <div v-if="needsDebug">
+      <span v-if="vizSpecs.needOptions">options: <br><pre><code>{{ options }}</code></pre></span><br>
+      fieldsForSeries: <br><pre><code>{{ fieldsForSeries }}</code></pre><br>
       <!-- minivizSettings : <br><code>{{ minivizSettings }}</code> -->
     </div>
   </div>
@@ -73,31 +101,49 @@ export default {
       // reRender: 1,
       availableVizTypes: {
         'simple-pie': {
-          type: 'pie'
+          type: 'pie',
+          needSerie: true,
+          needOptions: true,
+          needLabels: true
         },
         'simple-donut': {
-          type: 'donut'
-        },
-        'big-value': {
-          type: 'value'
-        },
-        'text-templated': {
-          type: 'value'
+          type: 'donut',
+          needSerie: true,
+          needOptions: true,
+          needLabels: true
         },
         'barchart-vertical': {
-          type: 'bar'
+          type: 'bar',
+          needSerie: true,
+          needOptions: true,
+          needSerieData: true,
+          needCategs: true,
+          horiz: false
         },
         'barchart-horizontal': {
-          type: 'bar'
+          type: 'bar',
+          needSerie: true,
+          needOptions: true,
+          needSerieData: true,
+          needCategs: true,
+          horiz: true
+        },
+        'big-values': {
+          needSerie: true,
+          needFieldName: true
+        },
+        'text-templated': {
         }
       },
-      // series: [44, 55, 13, 43, 22],
       series: undefined,
       options: undefined,
       chartOptionsDefault: {
         chart: {
           // type: 'pie',
           // width: 380
+          toolbar: {
+            show: false
+          }
         },
         // labels: ['Team A', 'Team B', 'Team C', 'Team D', 'Team E'],
         legend: {
@@ -114,19 +160,43 @@ export default {
             }
           }
         }]
-      }
+      },
+      // Only for debugging
+      vizTypeNeedsDebug: [
+        // 'simple-pie',
+        // 'simple-donut',
+        // 'barchart-vertical',
+        // 'barchart-horizontal',
+        'big-values'
+      ]
     }
   },
   computed: {
     vizType () {
       return this.minivizSettings.viztype
     },
-    fieldsForSerie () {
-      const fieldsForSerie = this.minivizSettings.serieFromField && this.minivizSettings.serieFromField.map(ffs => {
-        const field = this.fields.find(f => f.name === ffs.field)
-        return field
+    // only for debugging
+    needsDebug () {
+      return this.vizTypeNeedsDebug.includes(this.vizType)
+    },
+    vizSpecs () {
+      return this.availableVizTypes[this.vizType]
+    },
+    fieldsForSeries () {
+      // const fieldsForSeries = this.minivizSettings.seriesFromFields && this.minivizSettings.seriesFromFields.map(s => {
+      //   const serieFields = {
+      //     name: s.serieName[this.locale] || s.serieName.fr,
+      //     fields: s.fields.map(sf => {
+      //       return this.fields.find(f => f.name === sf.field)
+      //     })
+      //   }
+      //   return serieFields
+      // })
+      // return fieldsForSeries
+      const fieldsForSeries = this.minivizSettings.serieFromFields && this.minivizSettings.serieFromFields.map(sf => {
+        return this.fields.find(f => f.name === sf.field)
       })
-      return fieldsForSerie
+      return fieldsForSeries
     }
   },
   watch: {
@@ -135,33 +205,76 @@ export default {
     }
   },
   beforeMount () {
-    this.buildSeries()
-    this.buildOptions()
+    if (this.vizSpecs.needSerie) {
+      this.buildSerie()
+    }
+    if (this.vizSpecs.needOptions) {
+      this.buildOptions()
+    }
   },
   // mounted () {
   //   // this.reRender *= -1
   // },
   methods: {
-    buildSeries () {
-      // console.log('\nC-DatamiMiniviz > buildSeries > this.fieldsForSerie : ', this.fieldsForSerie)
+    buildSerie () {
+      this.needsDebug && console.log('\nC-DatamiMiniviz > buildSeries > this.fieldsForSeries : ', this.fieldsForSeries)
+      this.needsDebug && console.log('C-DatamiMiniviz > buildSeries > this.vizType : ', this.vizType)
       // console.log('C-DatamiMiniviz > buildSeries > this.item : ', this.item)
-      // console.log('C-DatamiMiniviz > buildSeries > this.fieldsForSerie : ', this.fieldsForSerie)
-      // const series = [44, 55, 13, 43, 22]
-      const series = this.fieldsForSerie.map(f => parseInt(this.item[f.field]))
+      // const series = [44, 55, 13, 43, 22] // example 1
+      // const series = { name: 'Serie name', data: [44, 55, 13, 43, 22] } // example 2
+      let series = this.fieldsForSeries && this.fieldsForSeries.map(f => {
+        return parseInt(this.item[f.field])
+      })
+      if (this.vizSpecs.needSerieData) {
+        series = [{
+          name: this.minivizSettings.serieName || '',
+          data: series
+        }]
+      }
+      if (this.vizSpecs.needFieldName) {
+        series = this.fieldsForSeries.map((f, i) => {
+          return {
+            field: f,
+            value: series[i]
+          }
+        })
+      }
+      this.needsDebug && console.log('C-DatamiMiniviz > buildSeries > series : ', series)
       this.series = series
     },
     buildLabels () {
-      // const labels = ['Team A', 'Team B', 'Team C', 'Team D', 'Team E']
-      const labels = this.fieldsForSerie.map(f => f.name)
+      this.needsDebug && console.log('C-DatamiMiniviz > buildLabels > this.fieldsForSeries : ', this.fieldsForSeries)
+      // const labels = ['Team A', 'Team B', 'Team C', 'Team D', 'Team E'] // example
+      const labels = this.fieldsForSeries && this.fieldsForSeries.map(f => {
+        return f.name
+      })
+      this.needsDebug && console.log('C-DatamiMiniviz > buildSeries > labels : ', labels)
       return labels
     },
+    buildCategs () {
+      const categs = this.fieldsForSeries.map(s => {
+        return s.name
+      })
+      return categs
+    },
     buildOptions () {
-      const viztype = this.availableVizTypes[this.vizType]
-      // console.log('\nC-DatamiMiniviz > buildOptions > viztype : ', viztype)
       const options = { ...this.chartOptionsDefault }
-      options.chart.type = viztype.type
-      options.labels = this.buildLabels()
-      // console.log('C-DatamiMiniviz > buildOptions > options : ', options)
+      options.chart.type = this.vizSpecs.type
+      if (this.vizSpecs.needLabels) {
+        options.labels = this.buildLabels()
+      }
+      if (this.vizSpecs.type === 'bar') {
+        options.plotOptions = {
+          bar: { horizontal: this.vizSpecs.horiz }
+        }
+      }
+      if (this.vizSpecs.needCategs) {
+        options.xaxis = {
+          categories: this.buildCategs(),
+          tooltip: { enabled: false }
+        }
+      }
+      this.needsDebug && console.log('C-DatamiMiniviz > buildOptions > options : ', options)
       this.options = options
     }
     // updateRender () {
